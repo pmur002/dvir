2c1f74c8a3fc5afbb76541395c1dcece0acd6717
Paul Murrell <paul@stat.auckland.ac.nz>
2021-02-25 12:22:31 +1300

    format code

--- R/ops.R
+++ R/ops.R
@@ -231,14 +231,18 @@ op_font_def <- function(op) {
 }
 
 identical_font <- function(op1, op2) {
-    
-    identical(blockValue(op1$blocks$op.opcode), blockValue(op2$blocks$op.opcode)) &&
-    identical(blockValue(op1$blocks$op.opparams.k), blockValue(op2$blocks$op.opparams.k)) &&
-    identical(blockValue(op1$blocks$op.opparams.c), blockValue(op2$blocks$op.opparams.c)) &&
-    identical(blockValue(op1$blocks$op.opparams.s), blockValue(op2$blocks$op.opparams.s)) &&
-    identical(blockValue(op1$blocks$op.opparams.d), blockValue(op2$blocks$op.opparams.d)) &&
-    identical(blockValue(op1$blocks$op.opparams.fontname.name), blockValue(op2$blocks$op.opparams.fontname.name))
-
+    identical(blockValue(op1$blocks$op.opcode),
+              blockValue(op2$blocks$op.opcode)) &&
+    identical(blockValue(op1$blocks$op.opparams.k),
+              blockValue(op2$blocks$op.opparams.k)) &&
+    identical(blockValue(op1$blocks$op.opparams.c),
+              blockValue(op2$blocks$op.opparams.c)) &&
+    identical(blockValue(op1$blocks$op.opparams.s),
+              blockValue(op2$blocks$op.opparams.s)) &&
+    identical(blockValue(op1$blocks$op.opparams.d),
+              blockValue(op2$blocks$op.opparams.d)) &&
+    identical(blockValue(op1$blocks$op.opparams.fontname.name),
+              blockValue(op2$blocks$op.opparams.fontname.name))
 }
 
 ## pre
